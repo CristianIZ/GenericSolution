fcc0061acb12c9b1dd7310a88fba033b0bb6393e
Zappala Cristian <czappala@hexacta.com>
2020-05-31 01:21:41 -0300

    esqueleto mejorado de aplicacion generica con webapp de angular

---
+++ Repository/Implementation/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Context;
+using Repository.Interfaces;
+
+namespace Repository.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SqlDatabaseContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public UnitOfWork(SqlDatabaseContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public int Complete()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        #region Ejemplo
+        //private readonly MessageTranslatorDbContext _dbContext;
+        //private readonly IMapper _mapper;
+        //private IDataTypeRepository _dataTypeRepository;
+        //private IEquivalenceRepository _equivalenceRepository;
+
+        //public INodeRepository NodeRepository
+        //{
+        //    get
+        //    {
+        //        if (_nodeRepository == null)
+        //        {
+        //            _nodeRepository = new NodeRepository(_dbContext, _mapper);
+        //        }
+        //        return _nodeRepository;
+        //    }
+        //}
+
+        //public IVerbRepository VerbRepository
+        //{
+        //    get
+        //    {
+        //        if (_verbRepository == null)
+        //        {
+        //            _verbRepository = new VerbRepository(_dbContext, _mapper);
+        //        }
+        //        return _verbRepository;
+        //    }
+        //}
+        #endregion
+
+    }
+}
